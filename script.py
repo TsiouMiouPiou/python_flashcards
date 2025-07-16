ca34e2272c933c363b8ff653fd17288303edf675
tsioumioupiou <g-anastasiou@hotmail.com>
2025-07-16 23:22:02 +0300

    Making the add_words better with deck addition

--- script.py
+++ script.py
@@ -15,16 +15,50 @@ except (FileNotFoundError, json.JSONDecodeError):
     words = {}  # start fresh if file not found or empty
 
 
+# def add_words():
+#     word = input('Enter a word: ').strip()
+#     definition = input('Enter a definition: ').strip()
+#     if word not in words:
+#         words[word] = definition
+#         json_object = json.dumps(words, indent=4)
+#         with open("flashcards.json", "w") as json_file:
+#             json_file.write(json_object)
+#     else:
+#         print("You already have that word!")
+
 def add_words():
-    word = input('Enter a word: ').strip()
-    definition = input('Enter a definition: ').strip()
-    if word not in words:
-        words[word] = definition
-        json_object = json.dumps(words, indent=4)
-        with open("flashcards.json", "w") as json_file:
-            json_file.write(json_object)
-    else:
-        print("You already have that word!")
+    prompt = str(input("Type 'n' for new deck or 'o' for old one ")).strip().lower()
+
+    if prompt == "n":
+        deck = str(input("Enter a deck name"))
+        if deck in words:
+            print("Deck already exists")
+        else:
+            words[deck] = {} # if deck is new initialize a dictionary
+
+        filling = True
+        while filling:
+            word = str(input('Enter a word: ')).strip()
+            if word not in words[deck]:
+                definition = input('Enter a definition: ').strip()
+                words[deck][word] = definition
+                json_object = json.dumps(words, indent=4)
+                with open("flashcards.json", "w") as json_file:
+                    json_file.write(json_object)
+            else:
+                print("This word already exists!")
+
+            more = str(input("Do you want to add more? (y/n) ")).strip().lower()
+            if more != "y":
+                filling = False
+
+
+    elif prompt == "o":
+         print(WindowsError)
+
+
+
+
 
 def delete_words():
     word = input('Delete a word: ').strip()
