4bd4ea3689a1cd2b3c7a645a77878290ff1877da
tsioumioupiou <g-anastasiou@hotmail.com>
2025-07-17 18:04:59 +0300

    I think I nailed the add_words() func

--- script.py
+++ script.py
@@ -1,5 +1,3 @@
-# TODO 1 = create a function that stores new words in a dictionary
-
 # TODO 3 = display all words in the dictionary
 # TODO 4 = predefine the article. if der die das then store it automatically
 # TODO 5 = create one dictionary for each type of word (noun, epi8eto, rima)
@@ -15,26 +13,14 @@ except (FileNotFoundError, json.JSONDecodeError):
     words = {}  # start fresh if file not found or empty
 
 
-# def add_words():
-#     word = input('Enter a word: ').strip()
-#     definition = input('Enter a definition: ').strip()
-#     if word not in words:
-#         words[word] = definition
-#         json_object = json.dumps(words, indent=4)
-#         with open("flashcards.json", "w") as json_file:
-#             json_file.write(json_object)
-#     else:
-#         print("You already have that word!")
-
 def add_words():
     prompt = str(input("Type 'n' for new deck or 'o' for old one ")).strip().lower()
 
     if prompt == "n":
         deck = str(input("Enter a deck name"))
-        if deck in words:
-            print("Deck already exists")
-        else:
-            words[deck] = {} # if deck is new initialize a dictionary
+        if deck not in words:
+            print(f'Deck "{deck}" is selected')
+            words[deck] = {}  # if deck is new initialize a dictionary
 
         filling = True
         while filling:
@@ -51,10 +37,26 @@ def add_words():
             more = str(input("Do you want to add more? (y/n) ")).strip().lower()
             if more != "y":
                 filling = False
-
-
     elif prompt == "o":
-         print(WindowsError)
+         deck = str(input("Enter an old deck"))
+         if deck in words:
+             print(f'Deck "{deck}" is selected')
+
+         filling = True
+         while filling:
+             word = str(input('Enter a word: ')).strip()
+             if word not in words[deck]:
+                 definition = input('Enter a definition: ').strip()
+                 words[deck][word] = definition
+                 json_object = json.dumps(words, indent=4)
+                 with open("flashcards.json", "w") as json_file:
+                     json_file.write(json_object)
+             else:
+                 print("This word already exists!")
+
+             more = str(input("Do you want to add more? (y/n) ")).strip().lower()
+             if more != "y":
+                 filling = False
 
 
 
@@ -73,13 +75,9 @@ def delete_words():
 
 # TODO 2 = create a function that starts the game
 def start():
-    start_program = True
-    while start_program:
+    add = str(input("Do you want to add words? (y/n) ")).strip().lower()
+    if add == "y":
         add_words()
-        more = str(input("Do you want to add more words? (y/n): ")).lower().strip()
-        if more == "y":
-            start_program = True
-        else:
-            start_program = False
+
 
 start()
